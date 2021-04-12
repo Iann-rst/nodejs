e12f0c6839c89e27808974ff03e96f6f8ea888b4
Iann-rst <iannrst11@gmail.com>
2021-04-12 18:49:51 -0300

    end status

---
+++ status.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+setInterval(() => {
+    const {freemem, totalmem} = os //extrair de dentro do os o freemem e totalmem
+
+    const total = parseInt(totalmem()/1028/1024)
+    const mem = parseInt(freemem()/1028/1024)
+    const percents = parseInt((mem/total)*100)
+    
+    const status = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    }
+
+    console.clear()
+    console.log("==== PC STATS ====")
+    console.table(status)
+
+}, 1000)
+
+
