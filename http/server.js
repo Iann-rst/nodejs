728e39338b07463a7aa8c0c4f9c34fd27d0781f8
Iann-rst <iannrst11@gmail.com>
2021-04-13 18:27:28 -0300

    add http

--- http/server.js
+++ http/server.js
@@ -11,6 +11,12 @@ http.createServer((req, res) => {
 
     const file = req.url=== '/'?  'index.html' : req.url
     const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if (!allowed) return
 
     fs.readFile(
         filePath, (err, content) => {
